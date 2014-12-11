f87aef346066e5f8bc3099e5e28f01a65c0f9f2f
Revaz Tatishvili <revaz.tatishvili@gmail.com>
2014-12-11 21:04:00 +0100

    extract window with filling edge values

--- common/image_op/image_manip.py
+++ common/image_op/image_manip.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from calc import fourier_calc
+from exceptions import RuntimeError
 
 
 def combine_magnitude_and_phase(ft_magnitude_image, ft_phase_image):
@@ -19,7 +20,7 @@ def combine_magnitude_and_phase(ft_magnitude_image, ft_phase_image):
 
     return np.fft.ifft2(combined_ft_image)
 
-def insert_beyond_edge(img, img_size, size, center, axis = 0):
+def insert_beyond_edge(img, img_size, size, center, mode, axis = 0):
     start = center - (size / 2)
     end = center + (size / 2 + 1)
 
@@ -40,19 +41,39 @@ def insert_beyond_edge(img, img_size, size, center, axis = 0):
     elif axis == 0:
         window = img[start:end, :]
     
-    while missing_col_count_start:
-        window = np.insert(window, 0, 0, axis = axis)
-        missing_col_count_start -= 1
-        print window
+    window = fill_missing(window, missing_col_count_start, size, 0, mode, axis)
+    window = fill_missing(window, missing_col_count_end, size, 1, mode, axis)
     
-    while missing_col_count_end:
-        window = np.insert(window, size - missing_col_count_end, 0, axis = axis)
-        missing_col_count_end -= 1
-        print window
-
     return window
 
-def extract_window(image, size, center):
+def fill_missing(window, iteration_count, size, window_side, mode, axis):
+    assert window_side == 0 or window_side == 1
+    
+    while iteration_count:
+        position = window_side * (size - iteration_count)
+        
+        if mode == 'constant' or mode == 'c':    
+            window = np.insert(window, position, 0, axis = axis)
+        elif mode == 'edge' or mode == 'e':    
+            
+            edge_index = window_side * (position - 1);
+            
+            if axis == 1:
+                new_edge = window[:, edge_index]
+            elif axis == 0:
+                new_edge = window[edge_index, :]
+                
+            window = np.insert(window, position, new_edge, axis = axis)
+            print window
+        else:
+             raise RuntimeError    
+        
+        iteration_count -= 1
+    
+    return window
+
+
+def extract_window(image, size, center, mode = 'constant'):
     """
     This method creates new image called "window" based on the original image
     where it is small square-shape portion of image where applicable and
@@ -68,8 +89,7 @@ def extract_window(image, size, center):
     assert(size[0] == size[1]), "size should have same values on both dimensions"
 
     window = image
-
-    window = insert_beyond_edge(window, window.shape[1], size[1], center[1], 1)
-    window = insert_beyond_edge(window, window.shape[0], size[0], center[0], 0)
+    window = insert_beyond_edge(window, window.shape[1], size[1], center[1], mode, 1)
+    window = insert_beyond_edge(window, window.shape[0], size[0], center[0], mode, 0)
 
     return window
