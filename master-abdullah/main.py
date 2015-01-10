1ad4142fc17b5a6f206e907e9c37ff51568ceeac
myrmidon <dada3mn@gmail.com>
2015-01-10 16:36:09 +0100

    v2_1: y dim sine; demo: double dim sine

--- master-abdullah/main.py
+++ master-abdullah/main.py
@@ -3,8 +3,10 @@ import numpy as np
 import plot
 import matplotlib.pyplot as plt
 
+
 def func_sin(x_arr, amplitude, frequency, phase=0):
     return amplitude * np.sin(2 * np.math.pi * frequency * x_arr + phase)
+
 
 def v1(display=True):
     f = read_image("Resources/bauckhage.jpg", as_array=True)
@@ -14,7 +16,7 @@ def v1(display=True):
     a = 15
     ph = 0.
     tp = f.shape[0] * 2.
-    freq = 1./tp
+    freq = 1. / tp
 
     x_out = a * np.sin(2 * np.math.pi * freq * x_in + ph)
 
@@ -30,7 +32,8 @@ def v1(display=True):
     if display:
         plot.plot_multiple_arrays([[g]], '', [''])
 
-def v2(freq_=1, img_path="Resources/asterixGrey.jpg", display=True):
+
+def v2(amplitude=64, freq_=1, img_path="Resources/asterixGrey.jpg", display=True, ret=False):
     """
     Full Sine waves in x direction
     :type img_path: path-string
@@ -38,7 +41,7 @@ def v2(freq_=1, img_path="Resources/asterixGrey.jpg", display=True):
     f = read_image(img_path, as_array=True)
     y_in, x_in = np.ogrid[0:f.shape[0], 0:f.shape[1]]
 
-    amp, freq, ph = 128, float(freq_)/(float(x_in.shape[1])), 0.
+    amp, freq, ph = amplitude, float(freq_) / (float(x_in.shape[1])), 0.
 
     y_out_diff = func_sin(x_in, amp, freq, ph).reshape(x_in.shape[1], 1).astype(int)
     y_out_main = y_in
@@ -55,7 +58,58 @@ def v2(freq_=1, img_path="Resources/asterixGrey.jpg", display=True):
     if display:
         plot.plot_multiple_arrays([[g]], '', [''])
 
+    if ret:
+        return g
+
+
+def v2_1(amplitude=64, wavelength=1, img=None, img_path="Resources/asterixGrey.jpg", display=True):
+    """
+    Full Sine Waves for y dimension
+    :param amplitude:
+    :param wavelength:
+    :param img:
+    :param img_path:
+    :param display:
+    :return:
+    """
+    if img is None:
+        f = read_image(img_path, as_array=True)
+    else:
+        f = img
+    y_in, x_in = np.ogrid[0:f.shape[0], 0:f.shape[1]]
+
+    amp, freq, ph = amplitude, 1. / float(y_in.shape[0] * wavelength), 0.
+
+    x_out_diff = func_sin(y_in.reshape(y_in.shape[1], y_in.shape[0]), amp, freq, ph).reshape(1, y_in.shape[0]).astype(
+        int)
+    x_out_main = x_in
+    y_out = y_in
+
+    g = np.zeros((y_out.shape[0], x_out_main.shape[1] + abs(x_out_diff.max()) + abs(x_out_diff.min())))
+
+    for j in xrange(y_out.shape[0]):
+        try:
+            g[j, x_out_main + x_out_diff.max() - x_out_diff[0, j]] = f[j, x_out_main]
+        except ValueError:
+            print j
+
+    if display:
+        plot.plot_multiple_arrays([[g]], '', [''])
+
+
+def demo(display=False):
+    """
+    Demo for double dim Sine Wave
+    :param display:
+    :return:
+    """
+    v2_1(wavelength=-1, amplitude=12, display=display,
+         img=v2(amplitude=12, freq_=8., img_path="Resources/bauckhage.jpg", display=False, ret=True))
+
+
 if __name__ == "__main__":
     # v1()
     # v2()
-    v2(freq_=7, img_path="Resources/bauckhage.jpg")
+    # v2(freq_=7, img_path="Resources/bauckhage.jpg")
+    # v2_1(wavelength=1/2.)
+    demo(display=True)
