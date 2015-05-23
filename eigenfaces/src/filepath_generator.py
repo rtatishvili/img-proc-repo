7fe6a5e9f107a24b5d263e2495760a0eb523d7b8
Revaz Tatishvili <revaz.tatishvili@gmail.com>
2015-05-23 21:05:39 +0200

    Loader for face images implemented

---
+++ eigenfaces/src/filepath_generator.py
@@ -0,0 +1,22 @@
+from math import log10, floor, ceil
+
+
+
+def count_zeros(number):
+    '''
+    Counts how many zeros will have the file name with particular number by file naming convention.
+    :param number: file name number
+    :return: number of zeros
+    '''
+    return 4 - int(floor(log10(number)))
+
+
+def get_filepath_for(number):
+    '''
+    Creates actual relative file path by file naming convention.
+    :param number: file name number
+    :return: relative file path
+    '''
+    zeros = count_zeros(number)        
+    
+    return "../train/face" + ("0" * zeros) + str(number) + ".pgm"     
---
+++ eigenfaces/test/test_load_images.py
@@ -0,0 +1,73 @@
+import unittest
+import numpy.testing as npt
+from src import image_loader, filepath_generator
+
+
+class Test(unittest.TestCase):
+
+
+    def test_load_image_loads_face_image(self):
+        filepath = "../train/face00001.pgm"
+        actual = image_loader.load_image(filepath)
+        expected = [104, 122, 142, 159, 162, 158, 167]
+                
+        npt.assert_array_equal(actual[0:7], expected, 'Image loader loads wrong image.')
+        
+    
+    def test_load_range_of_images(self):
+        images = image_loader.load_images(0, 10)
+
+        npt.assert_equal(images[0, 0:7], [104, 122, 142, 159, 162, 158, 167])
+        npt.assert_equal(images[1, 0:7], [112, 134, 160, 172, 183, 180, 184])
+        npt.assert_equal(images[8, 0:7], [ 64,  77,  85,  91, 101, 108, 112])
+        npt.assert_equal(images[9, 0:7], [ 50,  67,  77,  85,  90,  99, 107])
+
+
+    def test_loaded_images_shape(self):
+        images = image_loader.load_images(0, 10)
+        actual = images.shape
+        expected = (10, 361)
+        
+        npt.assert_equal(actual, expected)
+        
+        
+    def test_count_zeros_return_4_for_1_digit_number(self):
+        actual = filepath_generator.count_zeros(9)
+        expected = 4
+        
+        npt.assert_equal(actual, expected)    
+        
+
+    def test_count_zeros_return_3_for_2_digit_number(self):
+        actual = filepath_generator.count_zeros(10)
+        expected = 3
+        
+        npt.assert_equal(actual, expected)    
+ 
+ 
+    def test_count_zeros_return_2_for_3_digit_number(self):
+        actual = filepath_generator.count_zeros(100)
+        expected = 2
+        
+        npt.assert_equal(actual, expected)    
+
+
+    def test_filepath_counter_return_face00001_for_1(self):        
+        actual = filepath_generator.get_filepath_for(1)
+        expected = "../train/face00001.pgm"
+                         
+        npt.assert_equal(actual, expected)
+        
+                
+    def test_filepath_counter_return_face00016_for_16(self):
+        actual = filepath_generator.get_filepath_for(16)
+        expected = "../train/face00016.pgm"
+        
+        npt.assert_equal(actual, expected)
+        
+        
+        
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.test_load_image']
+    unittest.main()
